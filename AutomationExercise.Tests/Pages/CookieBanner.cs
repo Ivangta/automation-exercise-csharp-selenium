b4eef0938a9f49917ad5192afe19067379bf8917
Ivan G <ivanntu8@gmail.com>
2026-07-25 00:52:37 +0300

    Login invalid credentials test added.

---
+++ AutomationExercise.Tests/Pages/CookieBanner.cs
@@ -0,0 +1,19 @@
+﻿using AutomationExercise.Tests.Base;
+using OpenQA.Selenium;
+
+namespace AutomationExercise.Tests.Pages
+{
+    public class CookieBanner : BasePage
+    {
+        private readonly By acceptButton = By.CssSelector("button.fc-cta-consent");
+
+        public CookieBanner(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public void AcceptCookies()
+        {
+            Click(acceptButton);
+        }
+    }
+}
